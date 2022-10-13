47b4776bb742d33d385045093d30e613616609f0
gofrant <gofran178@gmail.com>
2022-10-13 10:23:28 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
+#include <studio.h>
 /**
- * main:Entry
+ *main -Entry
  * Return: Always 0
  */
 int main(void)
 {
-	prinft("size of a char : %i byte(s)\n", sizeof(char));
+	printf("size of char: %i byte(s)\n", sizeof(char));
+	printf("size of int: %i byte(s)\n", sizeof(int));
+	printf("size of long int: %i byte(s)\n", sizeof(long int));
+	printf("size of long long  int: %i byte(s)\n", sizeof(long long int));
+	printf("size of float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
