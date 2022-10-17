b218518fcfd769a5dc6ca95104185e149ddac7cf
gofran <gofran178@gmail.com>
2022-10-17 12:53:52 -0700

    66

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -print all comb of digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 50; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
