0f50c451f0fc164006acdb9d2eb7c6b910c5fdfe
gofran <gofran178@gmail.com>
2022-10-17 13:50:39 -0700

    v

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,6 +8,7 @@ int main(void)
 	int n;
 
 	for (n = 48; n < 50; n++)
+	{
 
 		putchar(n);
 
@@ -16,7 +17,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-
+	}
 		putchar('\n');
 
 		return (0);
