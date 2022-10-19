ecc16c2e32e9fdcfa42afa8ed528c68d7a4516d1
gofran <gofran178@gmail.com>
2022-10-19 16:47:44 -0700

    y

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: the first number
+ * @b: the secound number
+ * Return: The addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
