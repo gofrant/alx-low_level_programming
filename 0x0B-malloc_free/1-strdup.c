143f03f16956eca824d60e11099621d5140deb02
gofran <gofran178@gmail.com>
2022-11-09 03:18:42 -0800

    22

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,60 @@
+#include"main.h"
+/**
+ * _strln - count arry
+ * @s: array element
+ * Return: 1
+ */
+int _strln(char *s)
+{
+	unsigned int i;
+
+	i == 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+
+}
+/**
+ * _strcpy - copy array
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] != '\0';
+	return (dest);
+}
+/**
+ * _strdup - array of pointer
+ * @str: array of elements
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *dst;
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	size = _strlen(str) + i;
+	dst =  (char *) malloc(size * sizeof(char));
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+	strcpy(dst, str);
+	return (dst);
+}
