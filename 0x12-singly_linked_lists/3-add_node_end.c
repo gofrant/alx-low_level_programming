b9819e468982cddfe5baa40f611fd93f6e5771e2
gofran <gofran178@gmail.com>
2022-11-24 13:21:37 -0800

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,15 +2,14 @@
 /**
  * add_node - addes new node
  * @head: head of list
- * @str: node to be adde
+ * @str: node to be add
  * Return: on success the list if failed Null
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str);
 {
 	char *dup;
 	int len;
 	list_t *new;
-	size_t nchar;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -21,10 +20,20 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
-
 	for (len = 0; str[len]; len++)
 		new->str = dup;
 	new->len = len;
 	new->next = *head
+
+		if (*head == NULL)
+			*head = new;
+
+		else
+		{
+			last = *head;
+			while (last->next != NULL)
+				last = last->next;
+			last->next = new;
+		}
 	return (*head);
 }
